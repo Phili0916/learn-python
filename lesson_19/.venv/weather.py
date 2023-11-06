4fe9a897b7bab19c81ffe6e8c1b56dd2fdb817c4
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-11-06 16:57:14 +0100

    Weather app

--- lesson_19/.venv/weather.py
+++ lesson_19/.venv/weather.py
@@ -1,4 +1,27 @@
 import requests 
+import math
+import os
 from dotenv import load_dotenv
 
-request_url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric'
+from pprint import pprint
+
+
+
+load_dotenv()
+
+def get_current_weather():
+    print('\n*** Get Current Weather Conditions ***')
+
+    city = input('\n Please Enter a City Name: \n')
+
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
+    weather_data = requests.get(request_url).json()
+    # pprint(weather_data)
+    
+    print(f'\nThe current weather for {weather_data["name"]} is:')
+    print(f'\nThe current temperature is {round(weather_data["main"]["temp"])} degrees')
+    print(f'\nIt feels like {round(weather_data["main"]["feels_like"])} degrees with {weather_data["weather"][0]["description"]}.')
+
+if __name__ == "__main__":
+    get_current_weather()
