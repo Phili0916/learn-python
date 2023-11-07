3232d8b2d65affff596a184758bb40342ab0ba4a
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-11-07 10:33:44 +0100

    modify files

--- lesson_20/files.py
+++ lesson_20/files.py
@@ -7,7 +7,62 @@
 import os
 
 
-# Read - error if file doesn't exist
+##### Read - error if file doesn't exist #####
 
-f = open("names.txt")
+for f in os.listdir("/Users/Philip/python-projects/learn-python/lesson_20/"):
+    print(f)
+
+f = open("lesson_20/names.txt", "r")
 print(f.read())
+
+# print(f.readline())
+
+f.close()
+
+try:
+    f = open("does_not_exist", "r")
+    print(f.read())
+except:
+    print("File you want to read doesn't exist")
+finally:
+    f.close()
+
+##### Append - creates a file that doesn't exist #####
+
+f = open("lesson_20/names.txt", "a")
+f.write(f"\nSam")
+f.close()
+
+f = open("lesson_20/names.txt", "r")
+print(f.read())
+f.close()
+
+
+###### Write - You can overwrite in your file #####
+
+f = open("lesson_20/context.txt", "w")
+f.write("I can add text to my file")
+f.close()
+
+f = open("lesson_20/names.txt", "r")
+print(f.read())
+f.close()
+
+##### Two ways to create a new file #####
+
+# Opens a file for writing #
+
+f = open("lesson_20/new_names_list.txt", "w")
+f.close()
+
+# creates new file but will return an error if the file already exists
+if not os.path.exists("lesson_20/more_names.txt"):
+    f = open("lesson_20/more_names.txt", "x")
+    f.close()
+
+##### Delete a File #####
+
+if os.path.exists("lesson_20/new_names_list.txt"):
+    os.remove("lesson_20/new_names_list.txt")
+else:
+    print("The file you wish to delete does not exist")
